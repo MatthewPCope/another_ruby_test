77fea652cb09728030d2734452fb4884ecb7ead9
MatthewPCope <matthew.p.cope@gmail.com>
2024-01-02 18:27:20 -0800

    added registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-get "about", to: "about#index"
+  get "about", to: "about#index"
 
-root to: "main#index"
+  root to: "main#index"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_28_221159) do
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "string"
     t.string "password_digest"
     t.datetime "created_at", null: false
